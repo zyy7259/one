e0eb6381a0dfda81d0635f5e782df85b084b6411
Yingya Zhang <zyy7259@gmail.com>
2014-07-12 23:50:30 +0800

    Initial Commit

---
+++ one/ONEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ONEAppDelegate.h
+//  one
+//
+//  Created by zyy on 14-7-12.
+//  Copyright (c) 2014年 bird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ one/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  one
+//
+//  Created by zyy on 14-7-12.
+//  Copyright (c) 2014年 bird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ONEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ONEAppDelegate class]));
+    }
+}
