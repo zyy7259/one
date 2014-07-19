8e154c1d296e0452053dbfa420ec9000ef8808bb
Yingya Zhang <zyy7259@gmail.com>
2014-07-19 19:22:17 +0800

    ...

---
+++ one/ONERecommendationImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ONERecommendationImageViewController.h
+//  one
+//
+//  Created by zyy on 14-7-19.
+//  Copyright (c) 2014年 bird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONERecommendationImageViewController : UIViewController
+
+@end
---
+++ one/ONERecommendationImageViewController.m
@@ -0,0 +1,38 @@
+//
+//  ONERecommendationImageViewController.m
+//  one
+//
+//  Created by zyy on 14-7-19.
+//  Copyright (c) 2014年 bird. All rights reserved.
+//
+
+#import "ONERecommendationImageViewController.h"
+
+@interface ONERecommendationImageViewController ()
+
+@end
+
+@implementation ONERecommendationImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
